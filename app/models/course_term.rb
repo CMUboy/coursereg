ad49bd1b0031d1f2cb89e7783e89932bf97b03a2
Joseph Ku <jku@duettoresearch.com>
2015-05-17 19:07:01 -0700

    Add validations on models.

--- app/models/course_term.rb
+++ app/models/course_term.rb
@@ -1,3 +1,5 @@
 class CourseTerm < ActiveRecord::Base
+  validates :year, :season, presence: true
+
   enum season: {winter: 0, spring: 1, summer: 2, fall: 3}
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,6 @@
 class Enrollment < ActiveRecord::Base
+  validates :course_term, :student, :course, presence: true
+
   has_one :course_term
   belongs_to :student
   belongs_to :course
