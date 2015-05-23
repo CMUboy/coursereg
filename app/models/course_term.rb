cc4fb7e39265183965a5259492b7a0a1f314deea
Joseph Ku <jku@duettoresearch.com>
2015-05-22 20:53:37 -0700

    Add Bootstrap classes to HTML. Clean up generated scaffolding HTML code.

--- app/models/course_term.rb
+++ app/models/course_term.rb
@@ -2,4 +2,8 @@ class CourseTerm < ActiveRecord::Base
   validates :year, :season, presence: true
 
   enum season: {winter: 0, spring: 1, summer: 2, fall: 3}
+
+  def to_s
+    "#{season.titleize} #{year}"
+  end
 end
