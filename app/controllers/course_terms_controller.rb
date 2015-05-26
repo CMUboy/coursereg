de708446fad14acba05bfb84cc8382f2438bde0e
Joseph Ku <jku@duettoresearch.com>
2015-05-25 20:14:22 -0700

    Fix controller to allow saving and updating a model.

--- app/controllers/course_terms_controller.rb
+++ app/controllers/course_terms_controller.rb
@@ -69,6 +69,6 @@ class CourseTermsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_term_params
-      params[:course_term]
+      params.require(:course_term).permit(:season, :year)
     end
 end
